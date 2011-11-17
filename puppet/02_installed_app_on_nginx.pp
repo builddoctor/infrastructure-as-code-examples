1ad45a734556893eee1def8c5906d1bb59dbcf57
simpsonjulian <simpsonjulian@gmail.com>
2011-11-17 21:45:22 +0000

    now with nginx via vagrant

---
+++ puppet/02_installed_app_on_nginx.pp
@@ -0,0 +1,38 @@
+class nginx::install {
+  exec {
+    'aptitude update':
+      command => '/usr/bin/aptitude update';
+  }
+
+  package {
+    'nginx':
+      ensure  => present,
+      require => Exec['aptitude update']
+  }
+
+  file {
+    'default web page':
+      path => '/var/www/index.html',
+      content => 'This page, courtesy of puppet';
+      
+  }
+
+  service {
+    'nginx':
+      enable => true,
+      hasstatus => true,
+      ensure => running,
+      require => Package['nginx'];
+  }
+
+  group {
+    'puppet':
+      ensure => present;
+      # stop vagrant from moaning.
+    }
+
+}
+
+node default {
+  include nginx::install
+}
